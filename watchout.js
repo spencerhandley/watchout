a01e9db833d519e8c0d7c4565e03d407e1980ff8
spencer48 <spencerhandley@gmail.com>
2014-09-19 15:27:14 -0700

    Made enemies move to a new randome location using an easing function

--- watchout.js
+++ watchout.js
@@ -1,13 +1,24 @@
 // start slingin' some d3 here
 
 
-var width = 960,
-    height = 500;
+var width = 960;
+var height = 500;
+var numEnemies = 20;
+
+var randomCoords = function(n) {
+  var coords = [];
+  for (var i = 0; i < n; i++) {
+    var x = Math.floor(width * Math.random());
+    var y = Math.floor(height * Math.random());
+    coords.push([x,y]);
+  }
+  return coords;
+}
 
 var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height)
-    .attr("class", "gameFrame")
+    .attr("class", "gameFrame");
 
 
 var border = svg.append("rect")
@@ -19,10 +30,10 @@ var border = svg.append("rect")
     .style("fill", "none")
     .style("stroke-width", 2);
 
-var initPositions = [[50,50],[80,80],[100,150]];
+var coordStore = randomCoords(numEnemies);
 
-svg.selectAll("circle")
-   .data(initPositions)
+var enemies = svg.selectAll("circle")
+   .data(coordStore)
    .enter()
    .append("circle")
    .attr("cx", function(d) {
@@ -31,5 +42,25 @@ svg.selectAll("circle")
    .attr("cy", function(d) {
         return d[1];
    })
-   .attr("r", 4);
+   .attr("r", 10);
 
+
+var update = function(dataset){
+  // var newCoords = randomCoords(numEnemies)
+  svg.selectAll("circle")
+   .transition()
+     .duration(2000)
+     .attr("cx", function() {
+      return Math.floor((width) * Math.random());
+     })
+     .attr("cy", function() {
+      return Math.floor((height) * Math.random());
+     })
+     .ease()
+
+
+  //create new coordinates
+
+}
+
+setInterval(function(){update(coordStore)}, 2000);
